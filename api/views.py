952de1823e64c808a401624f59f7e27fddf5da2e
A-Burnhard <benadkojo@gmail.com>
2023-12-05 14:18:37 +0000

    updated

--- api/views.py
+++ api/views.py
@@ -69,55 +69,61 @@ qa_chain = RetrievalQA.from_chain_type(
 chat = ChatOpenAI(temperature=0.4, model=llm_model, openai_api_key=openai_api_key)
 class ProcessorView(APIView):
     def post(self, request):
-        serializer = DataSerializer(data=request.data)
-        if serializer.is_valid():
-            # Get the text from the request
-            input_text = serializer.validated_data['input_text']
+        try:
+            serializer = DataSerializer(data=request.data)
+            if serializer.is_valid():
+                # Get the text from the request
+                input_text = serializer.validated_data['input_text']
 
-            # Create a prompt for OpenAI using the input text
-            prompt = f"Please provide assistance as a receptionist of a tech company called Nerasol: \n```{input_text}```"
+                # Create a prompt for OpenAI using the input text
+                prompt = f"Please provide assistance as a receptionist of a tech company called Nerasol: \n```{input_text}```"
+                prompt_template = ChatPromptTemplate.from_template(prompt)
+                reception_messages = prompt_template.format_messages(
+                    style="Receptionist",
+                    text=input_text,
+                    max_tokens=500,
+                )
 
-            prompt_template = ChatPromptTemplate.from_template(prompt)
-            reception_messages = prompt_template.format_messages(
-                    style= "Receptionist",
-                    text= input_text,
-                    max_tokens= 500,
-                    )
-            
-           
+                # Use OpenAI to generate a response
+                response = chat(reception_messages)
 
-            # Use OpenAI to generate a response
-            response = chat(reception_messages)
+                # Extract the generated response from OpenAI
+                processed_text = response
 
-            # Extract the generated response from OpenAI
-            processed_text = response
+                # Create a new instance and save it with the processed text
+                data_instance = serializer.create({
+                    'processed_text': processed_text
+                })
 
-            # Create a new instance and save it with the processed text
-            data_instance = serializer.create({
-                'processed_text': processed_text
-            })
-
-
-            # Return the serialized data to the frontend
-            return Response(DataSerializer(data_instance).data)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                # Return the serialized data to the frontend
+                return Response(DataSerializer(data_instance).data)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            # Handle exceptions here, you can log the exception for debugging
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
 class ChatView(APIView):
-    def post(self,request):
-        serializer = DataSerializer(data=request.data)
-        if serializer.is_valid():
-            input_text = serializer.validated_data['input_text']
-            response = qa_chain(input_text)
-            response = response['result']
-            processed_text = response
+    def post(self, request):
+        try:
+            serializer = DataSerializer(data=request.data)
+            if serializer.is_valid():
+                input_text = serializer.validated_data['input_text']
+                response = qa_chain(input_text)
+                response = response['result']
+                processed_text = response
 
-            data_instance = serializer.create({
-                'processed_text': processed_text
-            })
-            return Response(DataSerializer(data_instance).data)
-        else:
+                data_instance = serializer.create({
+                    'processed_text': processed_text
+                })
+
+                return Response(DataSerializer(data_instance).data)
+
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as e:
+            # Handle the exception, you might want to log it or return a specific error response.
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class TestView(generics.CreateAPIView):
