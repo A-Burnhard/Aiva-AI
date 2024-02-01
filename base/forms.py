8c8ef81850faeb5710e0553ba53fdd695998b1c9
Appiedu Bernard <benadkojo@gmail.com>
2024-02-01 16:46:49 +0000

    Delete base directory

--- base/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Chat
-
-class Chat(forms.ModelForm):
-    class Meta:
-        model = Chat
-        fields = ['user_message',]
-        exclude = ['bot_response',]
