9a33ab415eb700d368f58b3381ff4d433650ab9e
Appiedu Bernard <benadkojo@gmail.com>
2024-02-01 16:40:15 +0000

    changed the allowed host

--- SR/settings.py
+++ SR/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7v8@tzw34k@2v1!u@zulz4(f-!u_x4%o@&mt6k5x-)7@@surel'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
